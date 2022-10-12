c4705c5faaa0c4cd67ba6c76b3500f23c712ca56
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2022-10-12 22:13:30 +0300

    Added implementation of the GetFunctionOfSetWindowCompositionAttrib method to the WindowEffectsConfigurator class

--- sources/window_effects_configurator.cpp
+++ sources/window_effects_configurator.cpp
@@ -17,4 +17,24 @@ void WindowEffectsConfigurator::ApplyEffects()
 
 	this->m_FunctionOfSetWindowCompositionAttribute(this->m_hwnd, &data);
 }
+
+void WindowEffectsConfigurator::GetFunctionOfSetWindowCompositionAttrib()
+{
+	const HMODULE user_module{GetModuleHandle(L"user32.dll")};
+
+	if(user_module)
+	{
+		this->m_FunctionOfSetWindowCompositionAttribute
+			= (WindowEffectsConfigurator::T_SetWindowCompositionAttribute)GetProcAddress(user_module, "SetWindowCompositionAttribute");
+
+		if (m_FunctionOfSetWindowCompositionAttribute == nullptr)
+		{
+			throw std::runtime_error("Can't get SetWindowCompositionAttribute function");
+		}
+	}
+	else
+	{
+		throw std::runtime_error("Can't get user32.dll module");
+	}
+}
 }  // namespace N_WindowEffectsConfigurator
