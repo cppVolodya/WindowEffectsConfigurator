8236bbc77275f442c304ac9c79b3562726641526
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2022-10-26 11:31:31 +0300

    added widgets to the MainWindow class

--- headers/main_window.hpp
+++ headers/main_window.hpp
@@ -36,6 +36,33 @@ private:
 	inline constexpr static int S_RANGE_OF_ALPHA_COLOR_CHANNEL_MAX{255};
 
 	inline constexpr static qreal S_ROUNDNESS_OF_WINDOW_SHAPE{20.0};
+private:
+	std::unique_ptr<WindowEffectsConfigurator> m_window_effects_configurator;
+
+	QPushButton *m_button_choose_color;
+	QPushButton *m_exit_button;
+
+	QComboBox *m_combo_box_of_effect_type;
+	QLabel    *m_label_of_effect_type;
+
+	QCheckBox *m_check_box_of_effect_config_of_enable_gradient_color;
+	QCheckBox *m_check_box_of_effect_config_of_expand_to_fullscreen;
+	QCheckBox *m_check_box_of_effect_config_of_enable_blur_behind_masks;
+
+	QSlider *m_slider_of_alpha_color_channel;
+	QLabel  *m_label_of_alpha_color_channel;
+
+	QCheckBox *m_check_box_of_transparent;
+	QCheckBox *m_check_box_of_frameless_window_hint;
+
+	QComboBox *m_combo_box_of_window_shape;
+	QLabel    *m_label_of_window_shape;
+
+	QColor m_chosen_color;
+
+	QPointF m_last_drag_position;
+
+	bool m_mouse_is_pressed;
 };
 }  // namespace N_WindowEffectsConfigurator
 
